f7309b5881c84ab3866eff9c3701f7cae68f87a0
Raqcc <raquelcutier@hotmail.com>
2024-06-17 20:09:23 -0300

    projeto final

--- Conta-Banco/src/ContaTermninal.java
+++ Conta-Banco/src/ContaTermninal.java
@@ -1,5 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTermninal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+       
+        String  nome = ("Raquel");
+        double saldo = (260.60);
+        
+        
+
+        
+         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite sua agencia");
+        String agencia = scanner.next();
+
+        System.out.println("Digite seu numero da conta");
+        int numero = scanner.nextInt();
+
+        System.out.println("Ola, + nome " + "Obrigado por criar uma conta em nosso Banco ");
+        System.out.println("Sua, + Agencia " + "numero da conta ");
+        System.out.println("seu, saldo é + saldo, para saque ");
     }
+
 }
